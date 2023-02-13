66c3d4ee7cc8c2d6611166198519bad3a5480407
YonadavM <yonadav120@gmail.com>
2023-02-13 11:44:49 +0200

    Made the text editing window

--- src/components/TextState.jsx
+++ src/components/TextState.jsx
@@ -1,8 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import io from 'socket.io-client';
+import Quill from 'quill';
+import 'quill/dist/quill.snow.css'
 
 export default function TextState() {
+    
+    
+    const wrapperRef = useCallback( (wrapper) => {
+        wrapper.innerHTML = ""
+        const editor = document.createElement('div')
+        wrapper.append(editor)
+        new Quill(editor, {theme: "snow"})
+    }, [])
+
     return (
-        <h1>Not ready yet...</h1>
+        <div id="text-editor-contianer" ref={wrapperRef}></div>
     );
 }
