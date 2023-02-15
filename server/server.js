dcaeb8ec8714754a575d4202af8d2087610e30b3
YonadavM <yonadav120@gmail.com>
2023-02-15 17:40:55 +0200

    Update server.js
     - update the server to check the computer ip and send it to the client

--- server/server.js
+++ server/server.js
@@ -1,5 +1,24 @@
+const os = require('os');
+
+const interfaces = os.networkInterfaces();
+let wifiAddress = null;
+
+for (const key in interfaces) {
+  if (key.includes('Wi-Fi') || key.includes('Wireless LAN')) {
+    for (const addr of interfaces[key]) {
+      if (addr.family === 'IPv4' && !addr.internal) {
+        wifiAddress = addr.address;
+        break;
+      }
+    }
+    break;
+  }
+}
+
 const io = require('socket.io')(5000, {
   cors: {
     origin: ['http://localhost:3000', 'http://localhost:3001']
   }
 })
+io.on('connection', socket => {
+  socket.emit('recive-ip-addr', wifiAddress)
